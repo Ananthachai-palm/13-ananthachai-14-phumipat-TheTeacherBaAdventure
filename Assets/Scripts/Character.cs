5771b905db2ea71155ee84a2d07aea8df2cbfb15
Ananthachai-palm <palmggxd@gmail.com>
2024-12-05 19:50:19 +0700

    Add Character, Player and PlayerControl scripts

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Charator : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
     // Field and Propoty
     private int _maxHP;
+    public int MaxHP
+    {
+        get { return _maxHP; }
+        set
+        {
+            if (value <= 0)
+            {
+                value = 1;
+            }
+            _maxHP = value;
+        }
+    }
     private int _hP;
     public int HP
     {
@@ -15,8 +27,7 @@ public abstract class Charator : MonoBehaviour
         }
         set
         {
-            Mathf.Clamp(value, 0, _maxHP);
-            _hP = value;
+            _hP = Mathf.Clamp(value, 0, _maxHP);
         }
     }
 
@@ -29,27 +40,43 @@ public abstract class Charator : MonoBehaviour
         }
         set
         {
+            if (value <= 0)
+            {
+                value = 1;
+            }
             _damage = value;
         }
     }
 
-    // Init
-    public void Init(int inputHP, int inputDamage)
-    {
-        _hP = inputHP;
-        _maxHP = inputHP;
+    protected Rigidbody2D _rb;
 
-        _damage = inputDamage;
+    // For set HP and Damage outside the scripts (Can set on unity)
+    /// <summary>
+    ///     Summary: if set value equal 0 or less, it will be 1. So please set value more than 0.
+    /// </summary>
+    [SerializeField] private int _setHP = 1;
+    [SerializeField] private int _setDamage = 1;
+
+    // Init
+    public void Init()
+    {
+        MaxHP = _setHP;
+        HP = MaxHP;
+
+        Damage = _setDamage;
+
+        _rb = GetComponent<Rigidbody2D>();
     }
     
     // Method
     public void TakeDamage(int inputDamage)
     {
-        _hP = inputDamage;
+        HP -= inputDamage;
     }
 
-    public bool IsDead()
+    // Use to check Dead
+    public virtual bool IsDead()
     {
-        return _hP <= 0; 
+        return HP == 0; 
     }
 }
